aa77610c8dfc9808ef94cd7403cc97f56a17b74c
YuutoIto <uplus.e10@gmail.com>
2015-05-23 00:20:56 +0900

    mv argv-parse codes to utils.rb

--- src/utils.rb
+++ src/utils.rb
@@ -1,3 +1,5 @@
+require 'optparse'
+
 class RenameRoutineError  < StandardError; end      #code bug
 class RenameStandardError < RenameRoutineError; end #この例外を補足する
 
@@ -19,4 +21,43 @@ end
 def goodbye(message)
   puts message
   exit(0)
+end
+
+#you can use regexp ^,$,?,* in before-string
+#エスケープしたくない文字もある
+def simple_escape!(str)
+  str.gsub!('(', '\(')
+  str.gsub!(')', '\)')
+  str.gsub!('[', '\[')
+  str.gsub!(']', '\]')
+  str.gsub!('{', '\{')
+  str.gsub!('}', '\}')
+  str.gsub!('+', '\+')
+  str.gsub!('.', '\.')
+  str.gsub!('?', '.')
+  str.gsub!('*', '.*?')
+  return str
+end
+
+def simple_escape(str)
+  simple_escape!(str.dup)
+end
+
+def argv_parse
+  ARGV[0] = '--help' if ARGV.size == 0
+  opt = { dire: nil, before: nil, after: nil }
+
+  parser = OptionParser.new
+  parser.banner = 'Usage: rename [directory] [target-string] [-ren]'
+  parser.on('-r before [after]',   HELP_MESSAGE[:replace]) do |before, after|
+    opt[:before] = before
+    opt[:after]  = after || ''
+  end
+  parser.parse!(ARGV)
+
+  opt[:dire] = ARGV[0] || './'
+  return opt
+
+  rescue OptionParser::MissingArgument, OptionParser::InvalidOption=> ex
+    error(ex.message, 10)
 end

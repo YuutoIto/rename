1168a746e447d03d45da20883627de6cc929efc3
YuutoIto <uplus.e10@gmail.com>
2015-05-23 01:37:21 +0900

    clean

--- src/rename-core.rb
+++ src/rename-core.rb
@@ -1,73 +1,48 @@
 require './utils'
 
 class TargetPathes
-	def initialize(dir_path, mode)
-		fail RenameStandard, "#{dir_path} not exists" unless Dir.exists?(dir_path)
+  def each(&block)
+    @names.each{ |oldname| block.call(oldname) }
+  end
 
-		@names = Dir.glob(dir_path.join("*"))
-		return if @names.empty?
+  def apply_name_pair
+    recursive_rename(@path_set)
+  end
 
-		case mode
-		when :all
-		when :file
-			@names.reject!{ |name| !File.file?(name) }
-		when :dir
-			@names.reject!{ |name| !File.directory?(name) }
-		else
-			raise RenameRoutineError, "#{mode} is invalid mode, can use :all, :file, :dir"
-		end
+  def set_name_pair(&block)
+    @path_set = []
+    @names.each do |oldname|
+      newname = block.call(oldname)
+      @path_set.push([@DIR_NAME.join(oldname), @DIR_NAME.join(newname)])
+    end
+    return nil
+  end
 
-		@names.map!{ |name| File.basename(name) }
-		@names.sort!
-		@DIR_NAME = dir_path
-	end
+  def safe_rename(path_pairs)
+    overlap = []
+    path_pairs.each do |oldpath, newpath|
+      if (!File.exists?(newpath))
+        File.rename(oldpath, newpath)
+      elsif (oldpath != newpath)
+        overlap.push([oldpath, newpath])
+      end
+    end
 
-	def each(&block)
-		@names.each{ |oldname| block.call(oldname) }
-	end
+    return overlap
+  end
 
-	def size
-		@names.size
-	end
+  def recursive_rename(path_pair)
+    count   = path_pair.size
+    overlap = self.safe_rename(path_pair) #renameして重複を得る
 
-	def apply_name_pair
-		recursive_rename(@path_set)
-	end
-
-	def set_name_pair(&block)
-		@path_set = []
-		@names.each do |oldname|
-			newname = block.call(oldname)
-			@path_set.push([@DIR_NAME.join(oldname), @DIR_NAME.join(newname)])
-		end
-		return nil
-	end
-
-	def safe_rename(path_pairs)
-		overlap = []
-		path_pairs.each do |oldpath, newpath|
-			if (!File.exists?(newpath))
-				File.rename(oldpath, newpath)
-			elsif (oldpath != newpath)
-				overlap.push([oldpath, newpath])
-			end
-		end
-
-		return overlap
-	end
-
-	def recursive_rename(path_pair)
-		count   = path_pair.size
-		overlap = self.safe_rename(path_pair) #renameして重複を得る
-
-		if (overlap.size == 0)
-			return true
-		elsif (overlap.size <  count) #重複が減っていれば
-			return self.recursive_rename(overlap)
-		elsif (overlap.size == count)	#重複が一つも解決しなければ
-			warn "Overlap: The following files did not rename, because already exists"
-			overlap.each {|old, new| puts "#{old} => #{new}" }
-			return false
-		end
-	end
+    if (overlap.size == 0)
+      return true
+    elsif (overlap.size <  count) #重複が減っていれば
+      return self.recursive_rename(overlap)
+    elsif (overlap.size == count) #重複が一つも解決しなければ
+      warn "Overlap: The following files did not rename, because already exists"
+      overlap.each {|old, new| puts "#{old} => #{new}" }
+      return false
+    end
+  end
 end
--- src/rename.rb
+++ src/rename.rb
@@ -10,32 +10,32 @@ pathes = enum_targets(opt)
 
 
 class Renamer < ARGVParser
-	def initialize(argv)
+  def initialize(argv)
     super(argv)
-	end
+  end
 
-	def parse
-		self.all_parse
-		@pathes  = TargetPathes.new(self.directory, :file)
-		@newword = NewWord.new(self.rename_option, @pathes.size)
-	end
+  def parse
+    self.all_parse
+    @pathes  = TargetPathes.new(self.directory, :file)
+    @newword = NewWord.new(self.rename_option, @pathes.size)
+  end
 
-	def rename
-		@pathes.set_name_pair do |oldname|
-			newname = simple_replace(oldname, self.target, @newword)
-			newname = oldname if newname.empty?
+  def rename
+    @pathes.set_name_pair do |oldname|
+      newname = simple_replace(oldname, self.target, @newword)
+      newname = oldname if newname.empty?
 
-			oldname = sprintf("%-32s", oldname) #インデントを揃える
-			puts "#{oldname}  =>  #{newname}"
-			next newname
-		end
+      oldname = sprintf("%-32s", oldname) #インデントを揃える
+      puts "#{oldname}  =>  #{newname}"
+      next newname
+    end
 
-		puts "Rename these? (y/N)"
-		case STDIN.gets.chomp
-		when /Y/i, /YES/i
-			@pathes.apply_name_pair
-		end
-	end
+    puts "Rename these? (y/N)"
+    case STDIN.gets.chomp
+    when /Y/i, /YES/i
+      @pathes.apply_name_pair
+    end
+  end
 end
 
 ##renamer entry point

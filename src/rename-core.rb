6cc47101fbc02b0e4eafd2d90758c9e5ab3cb96e
YuutoIto <uplus.e10@gmail.com>
2015-05-23 14:33:50 +0900

    Refactored the rename-core.rb.

--- src/rename-core.rb
+++ src/rename-core.rb
@@ -1,35 +1,33 @@
 require './utils'
 
-class TargetPathes
-  def apply_name_pair
-    recursive_rename(@path_set)
+# if newpath is not exists, oldpath to newpath.
+# return the overlap path.
+def safe_rename(path_pairs)
+  overlap = []
+  path_pairs.each do |old, new|
+    unless File.exists(new)
+      File.rename(old, new)
+    else
+      overlap.push([old, new])
+    end
   end
 
-  def safe_rename(path_pairs)
-    overlap = []
-    path_pairs.each do |oldpath, newpath|
-      if (!File.exists?(newpath))
-        File.rename(oldpath, newpath)
-      elsif (oldpath != newpath)
-        overlap.push([oldpath, newpath])
-      end
-    end
+  return overlap
+end
 
-    return overlap
-  end
-
-  def recursive_rename(path_pair)
-    count   = path_pair.size
-    overlap = self.safe_rename(path_pair) #renameして重複を得る
-
-    if (overlap.size == 0)
-      return true
-    elsif (overlap.size <  count) #重複が減っていれば
-      return self.recursive_rename(overlap)
-    elsif (overlap.size == count) #重複が一つも解決しなければ
-      warn "Overlap: The following files did not rename, because already exists"
-      overlap.each {|old, new| puts "#{old} => #{new}" }
-      return false
-    end
+# do safe_rename recursively
+# if all successful return true,
+# bad return false.
+def recursive_rename(path_pair)
+  overlap = self.safe_rename(path_pair)
+  case overlap.size
+  when 0
+    return true
+  when path_pair.size
+    warn "Overlap: The following files did not rename, because already exists"
+    overlap.each {|old, new| puts "#{old} => #{new}" }
+    return false
+  else
+    return recursive_rename(overlap)
   end
 end

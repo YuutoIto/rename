100b84c671b6771082b33f6409358f252efc35ae
YuutoIto <uplus.e10@gmail.com>
2015-05-28 20:25:11 +0900

    Update recursive_rename's spec

--- test/methods_spec.rb
+++ test/methods_spec.rb
@@ -12,10 +12,11 @@ describe "Rename spec" do
   end
   after(:all) do
     Dir.chdir("../")
-    # FileUtils.rm_r(TDIR) if File.exist?(TDIR)
+    FileUtils.rm_r(TDIR) if File.exist?(TDIR)
   end
 
-  let(:count) { Dir.glob("*").size }
+  let(:files) { Dir.glob("*") }
+  let(:count) { files.size }
 
   describe :safe_rename_pairs! do# {{{
     before(:all) { FileUtils.touch(%w[tmp.c out.c safe.c good.c]) }
@@ -26,13 +27,9 @@ describe "Rename spec" do
     context "if names is duplicate" do
       let(:pairs) { [["tmp.c", "out.c"], ["safe.c", "good.c"]] }
 
-      it "return is not empty" do
-        expect(Dir.glob("*").size).to eq 4
+      it "it is failure" do
         is_expected.to be_nil
-        expect(Dir.glob("*").size).to eq 4
-      end
-
-      it "rename failure" do
+        expect(count).to eq 4
         result = pairs.all? {|old, new| FileTest.exist?(old) && FileTest.exist?(new) }
         expect(result).to be_truthy
       end
@@ -41,9 +38,8 @@ describe "Rename spec" do
     context "if names is not duplicate" do
       let(:pairs) { [["tmp.c", "new1.c"], ["safe.c", "new2.c"]] }
       it "return is empty" do
-        expect(Dir.glob("*").size).to eq 4
         is_expected.to be_empty
-        expect(Dir.glob("*").size).to eq 4
+        expect(count).to eq 4
       end
 
       it "rename successfully" do
@@ -55,8 +51,8 @@ describe "Rename spec" do
 
   describe :recursive_rename! do
     names =  %w[00.c 01.c 02.c 03.c 04.c 05.c]
-    before(:all) { FileUtils.touch(names) }
-    after(:all)  { FileUtils.remove(Dir.glob("*")) }
+    before(:each) { FileUtils.touch(names) }
+    after(:each)  { FileUtils.remove(Dir.glob("*")) }
     subject { recursive_rename!(pairs) }
 
     #重複せずきれいに終わるパターン
@@ -64,10 +60,7 @@ describe "Rename spec" do
       let(:pairs) { [["00.c", "10.c"], ["01.c", "11.c"]] }
       it "it is successfully" do
         is_expected.to be_truthy
-      end
-
-      it "not changed file num" do
-        expect(Dir.glob("*").size).to eq names.size
+        expect(count).to eq names.size
       end
     end
 
@@ -75,18 +68,26 @@ describe "Rename spec" do
       #重複して解決しないパターン
       context "it is missing" do
         let(:pairs) { [["00.c", "01.c"], ["02.c", "01.c"]] }
-        it { is_expected.to be_falsey }
-        it { expect(Dir.glob("*").size).to eq names.size }
+        it {
+          old_out = $stdout.clone
+          old_err = $stderr.clone
+          $stdout = File.open("/dev/null", "w")
+          $stderr = File.open("/dev/null", "w")
+          is_expected.to be_falsey
+          expect(count).to eq names.size
+          $stdout = old_out
+          $stderr = old_err
+        }
       end
 
       #重複するが再帰で解決するパターン
       context "but it is successfully " do
         let(:pairs) { [["00.c", "01.c"], ["01.c", "21.c"]] }
-        it { is_expected.to be_falsey }
-        it { expect(Dir.glob("*").size).to eq names.size }
+        it {
+          is_expected.to be_truthy
+          expect(count).to eq names.size
+        }
       end
     end
-
-
   end
 end
